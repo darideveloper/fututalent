f233af9955b9209d88f1e04bd01c39cecc906bb4
daridev <darideveloper@gmail.com>
2024-08-07 11:20:11 -0600

    style: setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,6 +14,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "purple": "#671797",
+      "white": "#eae4ee",
+      "purple-light": "#9d48d0",
+      "purple-dark": "#4f0d4f",
+    }
   },
   plugins: [],
 };
