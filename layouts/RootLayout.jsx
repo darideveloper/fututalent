cbec5606c79f455a065e3d5944845a266a183741
daridev <darideveloper@gmail.com>
2024-08-12 11:36:14 -0600

    feat (RootLayout): render metadata

--- layouts/RootLayout.jsx
+++ layouts/RootLayout.jsx
@@ -1,18 +1,29 @@
 // import Footer from "@/sections/footer"
 import Header from "@/sections/header"
 import Contact from "@/sections/contact"
+import Head from "next/head"
+
 import { fontBody } from "@/libs/fonts"
+import { metaData } from "@/libs/meta"
 
 /**
  * Root layout component.
  * @param {object} props - component props
  * @param {JSX.Element} props.children - page content
+ * @param {string} props.pageTitle - title of current subpage
  * @returns {JSX.Element} - rendered Root layout
  */
-export default function RootLayout ({children}) {
+export default function RootLayout ({children, pageTitle}) {
 
   return (
     <>
+      {/* Metadata */}
+      <Head>
+        <title>{pageTitle ? `${pageTitle} | ${metaData.title}` : metaData.title}</title>
+        <meta name="description" content={metaData.description} />
+        <meta name="keywords" content={metaData.keywords} />
+        <meta name="author" content={metaData.author} />       
+      </Head>
       <Header />
       <main
         className={`
---
+++ libs/meta.js
@@ -0,0 +1,6 @@
+export const metaData = {
+  "title": "Fututalent",
+  "description": "Fututalent es....",
+  "keywords": "fututalent, fututalent es...",
+  "author": "darideveloper",
+}
