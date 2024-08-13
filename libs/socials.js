a019b8c2ca54949f208a1a13cf43c0101f509b7e
daridev <darideveloper@gmail.com>
2024-08-13 17:28:44 -0600

    style (Footer): update layout for single social button

--- libs/socials.js
+++ libs/socials.js
@@ -1,14 +1,6 @@
 export const socialsData = [
   {
-    "name": "facebook",
-    "link": "https://www.facebook.com"
-  },
-  {
-    "name": "instagram",
-    "link": "https://www.instagram.com"
-  },
-  {
     "name": "whatsapp",
-    "link": "https://www.whatsapp.com"
-  }
+    "link": "https://api.whatsapp.com/send?phone=5215515012783"
+  },
 ]
