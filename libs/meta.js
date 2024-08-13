737c2e3b6d23776006c93c28b923eab5c95a76c8
daridev <darideveloper@gmail.com>
2024-08-13 16:16:11 -0600

    feat (libs/meta): update data

--- libs/meta.js
+++ libs/meta.js
@@ -1,6 +1,6 @@
 export const metaData = {
   "title": "Fututalent",
-  "description": "Fututalent es....",
-  "keywords": "fututalent, fututalent es...",
+  "description": "En nuestra página web, podrás conocer más sobre nuestras iniciativas, así como explorar las diversas maneras en las que puedes contribuir a nuestra misión. En Fututalent estamos comprometidos en construir un camino hacia el éxito y la autonomía para cada uno de nuestros niños y jóvenes. ¡Conócenos!",
+  "keywords": "Inclusión, Desarrollo, Empoderamiento, Orientación, Mentoría, Capacitación, Oportunidades, Independencia, Vinculación, Apoyo, Resiliencia, Superación, Crecimiento, Transformación",
   "author": "darideveloper",
 }
