c3267cbcec36d267a5a92cf3b70aed4318d46ec3
daridev <darideveloper@gmail.com>
2024-08-13 14:44:00 -0600

    style: increase font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,15 @@ module.exports = {
       "purple-light": "#e4c4f7",
       "purple-dark": "#2c052c",
       "transparent": "transparent",
+    },
+    fontSize: {
+      sm: '1.6rem',
+      base: '2rem',
+      xl: '2.5rem',
+      '2xl': '3.126rem',
+      '3xl': '3.906rem',
+      '4xl': '4.88rem',
+      '5xl': '6.104rem',
     }
   },
   plugins: [],
