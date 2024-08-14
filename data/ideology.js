d8382d1c18e5214c7823fed286acf41d2865d179
daridev <darideveloper@gmail.com>
2024-08-14 13:06:48 -0600

    chore (data/ideology): update texts

--- data/ideology.js
+++ data/ideology.js
@@ -1,7 +1,7 @@
 export const idelogyData = [
   {
     "title": "origen",
-    "description": "Somos una organización dedicada al desarrollo de planes de vida y carrera para niños y jóvenes egresados de casas hogar. Nuestros origenes se remontan a 2018, cuando un grupo de amigos decidieron unir esfuerzos para ayudar a los niños y jóvenes de casas hogar a tener un futuro seguro e independiente."
+    "description": "Fututalent nace como una iniciativa por mejorar la calidad de vida de las infancias que residen en centros de asistencia social. Es así como una idea que nace a partir del cariño y aprecio hacia esta comunidad, se convierte en una organización social que crece a base de voluntariado, con el compromiso de dedicar su existencia a promover el desarrollo profesional en los niños."
   },
   {
     "title": "misión",
@@ -9,6 +9,6 @@ export const idelogyData = [
   },
   {
     "title": "visión",
-    "description": "Nuestra visión es ser la organización líder en el desarrollo de planes de vida y carrera para niños y jóvenes egresados de casas hogar en México. Queremos ser reconocidos por nuestra calidad y compromiso con la sociedad."
+    "description": "Aspiramos a construir un futuro en el que nuestros jóvenes, independientemente de su origen, tengan acceso a las herramientas y la motivación necesaria para alcanzar sus metas profesionales. Queremos ser la empresa social líder en desarrollo de talento."
   }
 ]
